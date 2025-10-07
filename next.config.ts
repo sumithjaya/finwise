71adead1c7b51c184b6cefd19dedd4ab64481c0e
SumithJayasuriya-FBS <44674003+sumithjaya@users.noreply.github.com>
2025-10-08 04:53:29 +0530

    up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
+const nextConfig: NextConfig = { 
    trailingSlash: true,
     images: {
     domains: ["localhost"], // allow images from your Strapi server
